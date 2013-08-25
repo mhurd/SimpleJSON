305a2c27554cf21fa0f2ac5c47e69a4bb2c8ab69
Mike Hurd <leahcim77@gmail.com>
2013-08-25 20:11:30 +0100

    Initial commit
    Includes the leksah workspace/package and module files.

---
+++ src/SimpleJSON.hs
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  SimpleJSON
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module SimpleJSON (
+
+) where
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+            deriving (Eq, Ord, Show)
+
+
+
+
